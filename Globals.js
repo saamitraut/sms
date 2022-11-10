07b7ae011832bb98a260f82e54edcde19fc5d86c
Akash <akg6595@gmail.com>
2022-11-10 19:07:19 +0530

    xxx

---
+++ Globals.js
@@ -0,0 +1,15 @@
+export default {
+  STORE_KEY: 'a56z0fzrNpl^2',
+  // BASE_URL: 'http://payment.iccnetwork.net/sms/',
+  // BASE_URL: 'http://103.219.0.103/',
+  BASE_URL: 'http://bill.seatvnetwork.com:8081/sla/',
+  COLOR: {
+    ORANGE: '#C50',
+    DARKBLUE: '#0F3274',
+    LIGHTBLUE: '#6EA8DA',
+    DARKGRAY: '#999',
+  },
+  // COMPANY_NAME: 'Sangli Media Communication',
+  // COMPANY_NAME: 'ICC   NETWORK',
+  COMPANY_NAME: 'SEATV  NETWORK',
+};
---
+++ react-native.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   project: {
+//     ios: {},
+//     android: {}, // grouped into "project"
+//   },
+//   assets: ['./assets/fonts/'], // stays the same
+// };
+
+
+
+module.exports = {
+  dependencies: {
+    'react-native-vector-icons': {
+      platforms: {
+        ios: null,
+      },
+      assets: ['./assets/fonts/'],
+    },
+  },
+};
